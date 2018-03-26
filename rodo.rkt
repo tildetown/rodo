f0222bb75516b2a72470f6b3304f5875d7df4da2
m455 <jessemilk00@gmail.com>
2018-03-26 13:43:17 -0400

    getting rid of hardcoded paths and names

--- rodo.rkt
+++ rodo.rkt
@@ -1,38 +1,45 @@
 #! /usr/bin/env racket
 #lang racket/base
 
+(define program-name "rodo")
+
+(define program-path "~/.")
+
 (define (displayln-hash-ref hash-list key)
   (displayln (hash-ref hash-list key)))
 
-(define messages (hash 
-                  'incorrect-usage "> For usage type `rodo -h` or `rodo --help`"
-                  'file-not-found "> rodo has not been setup in your home directory\nWould you like to set it up now? [y/n]"
-                  'file-exists "> .rodo file exists"
-                  'creating-file "> Creating .rodo file in your home directory..."
-                  'item-added "> Item added"
-                  'item-removed "> Item removed"
-                  'initializing "> Initializing rodo in your home directory"
-                  'terminating "> Terminating rodo..."
-                  'choose-y/n "> Error: Please choose y or n"))
+(define messages 
+  (hash 
+    'incorrect-usage (string-append "> For usage type `" program-name " -h` or `" program-name " --help`")
+    'file-not-found (string-append "> " program-name " has not been setup in your home directory\n> Would you like to set it up now? [y/n]")
+    'file-exists (string-append "> ." program-name " file exists")
+    'creating-file (string-append "> Creating ." program-name " file in your home directory...")
+    'item-added "> Item added"
+    'item-removed "> Item removed"
+    'initializing (string-append "> Initializing " program-name " in your home directory")
+    'terminating (string-append "> Terminating " program-name "...")
+    'choose-y/n "> Error: Please choose y or n"))
 
-(define y/n (hash
-              'yes '("yes" "Yes" "y" "Y")
-              'no '("no" "No" "n" "N")))
+(define y/n 
+  (hash
+    'yes '("yes" "Yes" "y" "Y")
+    'no '("no" "No" "n" "N")))
 
 (define (open/create-file path)
   (let ([path (expand-user-path path)])
     (let ([opened-file (open-output-file path
-                      #:mode 'text
-                      #:exists 'can-update)])
+                                         #:mode 'text
+                                         #:exists 'can-update)])
       (close-output-port opened-file))))
 
 (define (prompt-user prompt-message)
   (displayln-hash-ref messages prompt-message)
+  (display "> ")
   (let ([user-input (read-line)])
     (cond
       [(member user-input (hash-ref y/n 'yes))  
        (displayln-hash-ref messages 'creating-file)
-       (open/create-file "~/.rodo")]
+       (open/create-file (string-append program-path program-name))]
       [(member user-input (hash-ref y/n 'no))  
        (displayln-hash-ref messages 'terminating)]
       [else 
@@ -40,7 +47,7 @@
         (prompt-user 'file-not-found)])))
 
 (define (todo-list-exist?)
-  (if (file-exists? (expand-user-path "~/.rodo"))
+  (if (file-exists? (expand-user-path (string-append program-path program-name)))
     (displayln-hash-ref messages 'file-exists)
     (prompt-user 'file-not-found)))
 
