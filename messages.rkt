a379e585f5a28ba0d5b511a6a954fe767e7dbd59
m455 <jesselaprade@gmail.com>
2019-03-12 23:23:49 -0400

    made an awesome help message

--- messages.rkt
+++ messages.rkt
@@ -7,47 +7,73 @@
 (define messages
   (hash
    'show-help (string-append
-               "Usage:"
-               "\n"
-               "rodo <command> [optional argument]\n"
+               "Usage\n"
+               "======\n"
+               "If using the "
+               "<" config:initialize-command ">" " or "
+               "<" config:list-command ">" " commands, follow the structure below:\n"
+               config:program-name " <command>\n"
                "\n"
 
-               config:initialize-command ":\n"
-               "Initialize a file in "
+               "If using the "
+               "<" config:add-command ">" " or "
+               "<" config:remove-command ">" " commands, follow the structure below:\n"
+               config:program-name " <command> [argument]\n"
+               "\n"
+
+               "Commands\n"
+               "======\n"
+
+               "<" config:initialize-command ">" ":\n"
+               "Create a file in "
                config:program-directory
-               config:program-file "\n"
-
-               "Example: "
-               "rodo init\n"
+               config:program-file
+               " where your todo list will be stored\n"
+               "Example:\n"
+               config:program-name " "
+               config:initialize-command "\n"
                "\n"
 
-               config:list-command ":\n"
-               "Lists items on the list"
-               "\n"
-               "Example: "
-               "rodo ls\n"
-               "\n"
-
-               config:add-command ":\n"
-               "Adds an item to the list"
-               "\n"
-               "Example: "
-               "rodo add bread\n"
+               "<" config:list-command ">" ":\n"
+               "Displays items in the list\n"
+               "Example:\n"
+               config:program-name " "
+               config:list-command "\n"
                "\n"
 
-               "Note: For multi-word items you will need to\n"
-               "surround your item in double quotes as so:\n"
-               "rodo add \"go to the bank\"\n"
+               "<" config:add-command ">" " "
+               "[argument]:\n"
+               "Adds an item to the list\n"
+               "Example:\n"
+               config:program-name " "
+               config:add-command " "
+               "bread\n"
+               config:program-name " "
+               config:add-command " "
+               "this is an unquoted task\n"
+               config:program-name " "
+               config:add-command " "
+               "\"this is a quoted task which isn't any different from an unquoted task\"\n"
                "\n"
 
-               config:remove-command ":\n"
+               "<" config:remove-command ">" " "
+               "[argument]:\n"
                "Removes an item from the list\n"
-               "Example: "
-               "rodo rm 1\n"
+               "Example:\n"
+               config:program-name " " config:remove-command " " "1\n"
                "\n"
 
-               "Note: You may have to use the ls command to see\n"
-               "which number corresponds to which item\n")
+               "Note: You may have to run "
+               "`" config:program-name " " config:list-command "`" " "
+               "see which number corresponds to which item in the list."
+               " In the example above, the first item was removed from the list\n"
+               "\n"
+
+               "Can't see this whole help message?\n"
+               "======\n"
+               "Try running "
+               "`" config:program-name " -h | less` to use the arrow keys "
+               "to scroll up and down through the help message\n")
 
    'empty-todo-list "> There is nothing in your list \n"
 
