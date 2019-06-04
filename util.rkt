221c5bb8a46f6e1befdb1c955bf0af4131313a48
m455 <jesselaprade@gmail.com>
2019-06-03 22:01:34 -0400

    cleaning up comments

--- util.rkt
+++ util.rkt
@@ -28,7 +28,6 @@
 (define (display-hash-ref hash-list key)
   (display (hash-ref hash-list key)))
 
-;; Just so I don't have to keep typing "#:mode...#:line-mode..." every time
 (define (file->string-list config:path-to-file-to-file)
   (let ([todo-list (file:file->lines config:path-to-file-to-file
                                      #:mode 'text
@@ -38,13 +37,10 @@
 (define (list-empty? lst)
   (list:empty? (file->string-list lst)))
 
-;; Find out which item is being removed by scooping up
-;; the number the user entered in the command line
-;; arguments
 (define (get-removed-item lst args)
   ;; Subtract one from what the user chose, because they are
-  ;; are actually viewing the list numbers as human numbers
-  ;; so (actual-number +1)
+  ;; are actually viewing the list numbers as human numbers,
+  ;; so what they see is actual-number +1
   (list-ref (file->string-list lst) (sub1 (string->number args))))
 
 (define (surround-item-in-quotation-marks args)
@@ -56,7 +52,7 @@
 (define (prefix-item-with-number lst)
   (map string-append
        ;; Note: compose starts from the last element in it's
-       ;; list, as if it were nested, so that would be add1 here
+       ;; list, so that would be add1 here
        (map (compose prefix-item-with-period number->string add1)
             (list:range (length lst)))
        lst))
@@ -87,33 +83,30 @@
           (check-for-program-directory)
           (check-for-program-file))
          (if
-          ;; If file exists, see if it's empty, if so
-          ;; tell the user
           (list-empty? config:path-to-file)
           (display-hash-ref messages:messages 'empty-todo-list)
-          ;; If file isn't empty, display a pretty list
           (display-prettified-program-file))]
-        ;; If file doesn't exist, tell the user
         [else
          (display-hash-ref messages:messages 'file-not-found)
          (display-hash-ref messages:messages 'try-init)]))
 
 (define (write-item-to-program-file args)
-  ;; Add item to end of list and write to file
+  ;; Add an item to the end of a list and write to a file
   (let ([new-list (append-item-to-end args config:path-to-file)])
     (file:display-to-file
-     (string:string-join new-list "\n")
-     config:path-to-file
+     (string:string-join new-list "\n") config:path-to-file
      #:mode 'text
      #:exists 'truncate)
+    ;; After writing to the file, tell the user what was written
     (display-item-added args)))
 
 (define (add-item-to-list args)
-  (if (and
-       (check-for-program-directory)
-       (check-for-program-file))
-      ;; The cdr here prevents the command line argument "add" from being added to the file
+  (if (and (check-for-program-directory)
+           (check-for-program-file))
+      ;; The cdr here prevents the first command line argument ("add")
+      ;; from being added to the file
       (write-item-to-program-file (string:string-join (cdr (vector->list args))))
+      ;; Else
       (begin
         (display-hash-ref messages:messages 'file-not-found)
         (display-hash-ref messages:messages 'try-init))))
@@ -131,11 +124,11 @@
 (define (remove-item-from-list args)
   (cond [(list-empty? config:path-to-file)
          (display-hash-ref messages:messages 'empty-todo-list)]
-        [(and
-          (check-for-program-directory)
-          (check-for-program-file))
+        [(and (check-for-program-directory)
+              (check-for-program-file))
          (remove-item-from-program-file (vector-ref args 1))]
-        [(and (not (check-for-program-directory)) (not (check-for-program-file)))
+        [(and (not (check-for-program-directory))
+              (not (check-for-program-file)))
          (begin
            (display-hash-ref messages:messages 'file-not-found)
            (display-hash-ref messages:messages 'try-init))]))
